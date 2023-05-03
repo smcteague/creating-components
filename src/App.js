658fe9dfc65825ccb0349a90e7596463e256ab5c
Steve McTeague <stevemcteague@gmail.com>
2023-05-03 13:33:38 -0400

    Added sample code.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return <h1>Hello world</h1>;
+}
+
 function App() {
-   
+  return <Header />;
 }
 
 export default App;
